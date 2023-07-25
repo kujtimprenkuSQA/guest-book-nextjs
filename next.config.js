8d4cb06ddb810d8a8e6e050f00c212640cfc4f20
Kujtim Prenku <kujtim.prenku@sqa-consulting.com>
2023-07-25 16:02:42 +0200

    Transpile @near-wallet-selector/wallet-connect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["@near-wallet-selector/wallet-connect"],
+  experimental: {
+    esmExternals: "loose"
+  }
 }
 
 module.exports = nextConfig
