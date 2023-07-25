bbe6e84905f81872abda6febeb9b1042d4199da1
Kujtim Prenku <kujtim.prenku@sqa-consulting.com>
2023-07-25 15:56:51 +0200

    Comment other options use only wallet-connect.

--- contexts/WalletSelectorContext.tsx
+++ contexts/WalletSelectorContext.tsx
@@ -1,31 +1,31 @@
-import { setupCoin98Wallet } from "@near-wallet-selector/coin98-wallet";
+// import { setupCoin98Wallet } from "@near-wallet-selector/coin98-wallet";
 import type { AccountState, WalletSelector } from "@near-wallet-selector/core";
 import { setupWalletSelector } from "@near-wallet-selector/core";
-import { setupHereWallet } from "@near-wallet-selector/here-wallet";
-import { setupMathWallet } from "@near-wallet-selector/math-wallet";
-import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
-import { setupNarwallets } from "@near-wallet-selector/narwallets";
+// import { setupHereWallet } from "@near-wallet-selector/here-wallet";
+// import { setupMathWallet } from "@near-wallet-selector/math-wallet";
+// import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
+// import { setupNarwallets } from "@near-wallet-selector/narwallets";
 import type { WalletSelectorModal } from "@near-wallet-selector/modal-ui";
 import { setupModal } from "@near-wallet-selector/modal-ui";
-import { setupNearWallet } from "@near-wallet-selector/near-wallet";
-import { setupNearFi } from "@near-wallet-selector/nearfi";
-import { setupNightly } from "@near-wallet-selector/nightly";
-import { setupNightlyConnect } from "@near-wallet-selector/nightly-connect";
-import { setupSender } from "@near-wallet-selector/sender";
+// import { setupNearWallet } from "@near-wallet-selector/near-wallet";
+// import { setupNearFi } from "@near-wallet-selector/nearfi";
+// import { setupNightly } from "@near-wallet-selector/nightly";
+// import { setupNightlyConnect } from "@near-wallet-selector/nightly-connect";
+// import { setupSender } from "@near-wallet-selector/sender";
 import { setupWalletConnect } from "@near-wallet-selector/wallet-connect";
-import { setupNearSnap } from "@near-wallet-selector/near-snap";
-import { setupWelldoneWallet } from "@near-wallet-selector/welldone-wallet";
-import { setupXDEFI } from "@near-wallet-selector/xdefi";
+// import { setupNearSnap } from "@near-wallet-selector/near-snap";
+// import { setupWelldoneWallet } from "@near-wallet-selector/welldone-wallet";
+// import { setupXDEFI } from "@near-wallet-selector/xdefi";
 import type { ReactNode } from "react";
 import React, { useCallback, useContext, useEffect, useState } from "react";
 import { distinctUntilChanged, map } from "rxjs";
 
-import { setupNeth } from "@near-wallet-selector/neth";
-import { setupOptoWallet } from "@near-wallet-selector/opto-wallet";
-import { setupFinerWallet } from "@near-wallet-selector/finer-wallet";
+// import { setupNeth } from "@near-wallet-selector/neth";
+// import { setupOptoWallet } from "@near-wallet-selector/opto-wallet";
+// import { setupFinerWallet } from "@near-wallet-selector/finer-wallet";
 import { Loading } from "../components/Loading";
-import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
-import { setupLedger } from "@near-wallet-selector/ledger";
+// import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
+// import { setupLedger } from "@near-wallet-selector/ledger";
 import { CONTRACT_ID } from "../constants";
 
 declare global {
@@ -57,26 +57,26 @@ export const WalletSelectorContextProvider: React.FC<{
       network: "testnet",
       debug: true,
       modules: [
-        setupMyNearWallet(),
-        setupLedger(),
-        setupNearWallet(),
-        setupSender(),
-        setupMathWallet(),
-        setupNightly(),
-        setupMeteorWallet(),
-        setupNearSnap(),
-        setupNarwallets(),
-        setupWelldoneWallet(),
-        setupHereWallet(),
-        setupCoin98Wallet(),
-        setupNearFi(),
-        setupNeth({
-          gas: "300000000000000",
-          bundle: false,
-        }),
-        setupOptoWallet(),
-        setupFinerWallet(),
-        setupXDEFI(),
+        // setupMyNearWallet(),
+        // setupLedger(),
+        // setupNearWallet(),
+        // setupSender(),
+        // setupMathWallet(),
+        // setupNightly(),
+        // setupMeteorWallet(),
+        // setupNearSnap(),
+        // setupNarwallets(),
+        // setupWelldoneWallet(),
+        // setupHereWallet(),
+        // setupCoin98Wallet(),
+        // setupNearFi(),
+        // setupNeth({
+        //   gas: "300000000000000",
+        //   bundle: false,
+        // }),
+        // setupOptoWallet(),
+        // setupFinerWallet(),
+        // setupXDEFI(),
         setupWalletConnect({
           projectId: "c8cb6204543639c31aef44ea4837a554",
           metadata: {
@@ -86,15 +86,15 @@ export const WalletSelectorContextProvider: React.FC<{
             icons: ["https://avatars.githubusercontent.com/u/37784886"],
           },
         }),
-        setupNightlyConnect({
-          url: "wss://relay.nightly.app/app",
-          appMetadata: {
-            additionalInfo: "",
-            application: "NEAR Wallet Selector",
-            description: "Example dApp used by NEAR Wallet Selector",
-            icon: "https://near.org/wp-content/uploads/2020/09/cropped-favicon-192x192.png",
-          },
-        }),
+        // setupNightlyConnect({
+        //   url: "wss://relay.nightly.app/app",
+        //   appMetadata: {
+        //     additionalInfo: "",
+        //     application: "NEAR Wallet Selector",
+        //     description: "Example dApp used by NEAR Wallet Selector",
+        //     icon: "https://near.org/wp-content/uploads/2020/09/cropped-favicon-192x192.png",
+        //   },
+        // }),
       ],
     });
     const _modal = setupModal(_selector, {
